d27fbff6894a6d32c0159f183aa8bfa65f687bca
Adınız Soyadınız <email@example.com>
2025-04-08 17:55:16 +0300

    sayfa numaralandırma

--- src/js/utilities/pagination.js
+++ src/js/utilities/pagination.js
@@ -12,3 +12,32 @@ function resizeVisPage() { // Çöznürlüğe göre bölüm sayısı
         return 3;
     }
 }
+// Sayfalama - Pagination
+function startPagination(page, perPage, totalPages, callback) {
+    const options = {
+        totalItems: Number(perPage) * Number(totalPages),
+        itemsPerPage: Number(perPage),
+        visiblePages: resizeVisPage(),
+        page: Number(page),
+        centerAlign: false,
+        omitMiddlePages: false,
+        firstItemClassName: 'tui-first-child',
+        lastItemClassName: 'tui-last-child',
+        template: {
+            page: '<a href="#" class="tui-page-btn pag-page">{{page}}</a>',
+            currentPage:
+                '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+            moveButton:
+                '<a href="#" class="tui-page-btn tui-{{type}} move-button">' +
+                '<span class="tui-ico-{{type}}">{{type}}</span>' +
+                '</a>',
+            disabledMoveButton:
+                '<span class="tui-page-btn tui-is-disabled tui-{{type}} prev-button">' +
+                '<span class="tui-ico-{{type}}">{{type}}</span>' +
+                '</span>',
+            moreButton:
+                '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip more-button">' +
+                '<span class="tui-ico-ellip">...</span>' +
+                '</a>',
+        },
+    };
